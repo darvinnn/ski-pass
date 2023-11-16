d2bb75a58104e390e5a39ccc61cb87dddbdb981c
Николаев Максим <darwinnnn27@gmail.com>
2023-11-16 12:49:17 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  console.log('BAMBAM')
+  console.log('BAMBAM');
 
   return (
     <>
